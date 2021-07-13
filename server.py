a9e7404b68921aabef13857fe0229cc9d834a67f
Maksymilian Biruk <mexberuk@gmail.com>
2021-07-14 01:35:13 +0200

    add PublicConnectionManager class

--- server.py
+++ server.py
@@ -2,3 +2,24 @@ from fastapi import FastAPI, WebSocket
 
 
 app = FastAPI()
+
+
+class PublicConnectionManager:
+    '''
+    Stands for connection to the public chat room
+    without limiting amount of connected clients
+    '''
+
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
