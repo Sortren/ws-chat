b2b57fadedff31c27e333569b2a31f29b47c740d
Maksymilian Biruk <mexberuk@gmail.com>
2021-07-18 02:37:15 +0200

    uncomment public_chat endpoint, add counter_broadcast to body

--- src/server.py
+++ src/server.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketDisconnect
-from managers import PrivateConnectionManager
+from managers import PublicConnectionManager, PrivateConnectionManager
 
 app = FastAPI()
 
-# public_manager = PublicConnectionManager()
+public_manager = PublicConnectionManager()
 private_manager = PrivateConnectionManager()
 
 
-# @app.websocket("/chat/public")
-# async def public_chat(websocket: WebSocket):
-#     await public_manager.connect(websocket)
-#     await public_manager.greeting_broadcast(websocket)
+@app.websocket("/chat/public")
+async def public_chat(websocket: WebSocket):
+    await public_manager.connect(websocket)
+    await public_manager.counter_broadcast()
+    await public_manager.greeting_broadcast(websocket)
 
-#     try:
-#         while True:
-#             data = await websocket.receive_json()
-#             await public_manager.broadcast(f"{data.get('username')}> {data.get('message')}")
-#     except WebSocketDisconnect:
-#         await public_manager.disconnect(websocket)
-#         await public_manager.broadcast(f"Someone left the chat!")
+    try:
+        while True:
+            data = await websocket.receive_json()
+            await public_manager.broadcast(data)
+    except WebSocketDisconnect:
+        await public_manager.disconnect(websocket)
+        await public_manager.counter_broadcast()
+        await public_manager.broadcast(f"Someone left the chat!")
 
 
 @app.websocket("/chat/private")
